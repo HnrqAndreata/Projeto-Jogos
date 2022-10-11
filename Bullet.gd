c527ead3907f2839a95ef2790574fe38998164bd
HnrqAndreata <henrique.andreata@edu.pucrs.br>
2022-10-11 09:20:06 -0300

    movimentacao ok e bala criada

---
+++ Bullet.gd
@@ -0,0 +1,22 @@
+extends KinematicBody2D
+
+
+# Declare member variables here. Examples:
+# var a: int = 2
+# var b: String = "text"
+onready var sprite := $Sprite
+onready var speed := 10
+
+func move():
+	if(sprite.)
+		velocity.x = speed
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta: float) -> void:
+#	pass
---
+++ Player.gd
@@ -0,0 +1,38 @@
+extends KinematicBody2D
+
+export (int) var speed = 100
+export (float) var rotation_speed = 4
+export (int) var jump_speed = 1000
+export (int) var gravity = 3000
+
+
+onready var target := position
+onready var sprite := $Sprite
+
+var velocity := Vector2.ZERO
+var rotation_dir := 0
+
+func get_side_input():
+	velocity.x = Input.get_action_strength("ui_right")-Input.get_action_strength("ui_left")
+	velocity.x *= speed
+	if velocity.x > 0:
+		sprite.play("right")
+		sprite.scale.x = abs(sprite.scale.x)
+	elif velocity.x < 0:
+		sprite.play("left")
+		sprite.scale.x = -1 * abs(sprite.scale.x)
+	elif Input.get_action_strength("ui_accept"):
+		sprite.play("shoot")
+	else:
+		sprite.play("idle")
+		 
+	if is_on_floor() and Input.is_action_just_pressed('ui_up'):
+		sprite.play("jump")
+		velocity.y = -jump_speed
+		
+func _physics_process(delta):
+
+	velocity.y += gravity * delta
+	get_side_input()
+	
+	velocity = move_and_slide(velocity, Vector2.UP)
